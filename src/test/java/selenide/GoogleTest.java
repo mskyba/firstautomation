0a8f525293b94557d6b59026c777a039e5f01d8e
Mykola <mbskyba@gmail.com>
2022-08-11 14:06:24 +0300

    restAssured 2nd-commit other files commented

--- src/test/java/selenide/GoogleTest.java
+++ src/test/java/selenide/GoogleTest.java
@@ -1,48 +1,48 @@
-package selenide;
-
-import com.codeborne.selenide.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-import pages.google.GoogleHomePage;
-
-import static com.codeborne.selenide.CollectionCondition.*;
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.*;
-
-public class GoogleTest {
-
-    @BeforeClass
-    public void setUp() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.savePageSource = false;
-        Configuration.timeout = 4000;//по умолчанию
-        Configuration.browserSize = "1920x1080";
-    }
-
-    @Test
-    public void userShouldSearch() {
-        open("https://google.com");
-        GoogleHomePage googleHomePage = new GoogleHomePage();
-        googleHomePage.waitUntilSearchFieldDisplayed()
-                .setSearchText("Selenide")
-                .pressEnter();
-
-
-
-
-//        googleHomePage.searchField
-//                .shouldBe(visible)
-//                .setValue("Selenide")
+//package selenide;
+//
+//import com.codeborne.selenide.*;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.Test;
+//import pages.google.GoogleHomePage;
+//
+//import static com.codeborne.selenide.CollectionCondition.*;
+//import static com.codeborne.selenide.Condition.*;
+//import static com.codeborne.selenide.Selenide.*;
+//
+//public class GoogleTest {
+//
+//    @BeforeClass
+//    public void setUp() {
+//        Configuration.holdBrowserOpen = true;
+//        Configuration.savePageSource = false;
+//        Configuration.timeout = 4000;//по умолчанию
+//        Configuration.browserSize = "1920x1080";
+//    }
+//
+//    @Test
+//    public void userShouldSearch() {
+//        open("https://google.com");
+//        GoogleHomePage googleHomePage = new GoogleHomePage();
+//        googleHomePage.waitUntilSearchFieldDisplayed()
+//                .setSearchText("Selenide")
 //                .pressEnter();
-//        $$x("//h3[contains(text(), 'Selenide')]")
-//                .filter(visible)
-//                .shouldHave(sizeGreaterThanOrEqual(7))
-//                .get(0)
-//                .click();
-//        $(".donate_header")
-//                .shouldHave(text("Selenide Supports Ukraine \uD83C\uDDFA\uD83C\uDDE6"));
-    }
-}
+//
+//
+//
+//
+////        googleHomePage.searchField
+////                .shouldBe(visible)
+////                .setValue("Selenide")
+////                .pressEnter();
+////        $$x("//h3[contains(text(), 'Selenide')]")
+////                .filter(visible)
+////                .shouldHave(sizeGreaterThanOrEqual(7))
+////                .get(0)
+////                .click();
+////        $(".donate_header")
+////                .shouldHave(text("Selenide Supports Ukraine \uD83C\uDDFA\uD83C\uDDE6"));
+//    }
+//}
