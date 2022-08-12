14628ecb9f6c6c58f570946fc8fe6c8721d99ee2
Mykola <mbskyba@gmail.com>
2022-08-12 19:53:08 +0300

    exceptions

---
+++ src/main/java/check/Main.java
@@ -0,0 +1,10 @@
+package check;
+
+public class Main {
+    public static void main(String[] args) {
+        new Checking();
+        Checking.checkLogInInfo("it-hillel", "hillel", "hillel");
+
+    }
+}
+
